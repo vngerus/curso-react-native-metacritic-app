67e959d095b98c740adbcf913d2b1f29944e1f80
angel <angelsmithlgs@gmail.com>
2024-07-25 19:31:08 -0400

    build: initial structure

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Image source={{ uri: "https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg" }} style={{ width: 215, height: 294 }} />
+      <Text>Testing app</Text>
+      <Button
+        title='Pulsa aquí'
+        onPress={() => alert('Hola')} />
     </View>
   );
 }
